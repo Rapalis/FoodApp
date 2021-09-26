9353193ecc91cdcc141040ba93c43dfab74967a1
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-26 23:21:42 +0300

    Updated providerDTO

--- FoodApp/Models/DataTransferObjects/Responses/ProviderDTO.cs
+++ FoodApp/Models/DataTransferObjects/Responses/ProviderDTO.cs
@@ -2,9 +2,7 @@
 {
     public class ProviderDTO
     {
-        public string Email { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }
         public string Address { get; set; }
     }
 }
