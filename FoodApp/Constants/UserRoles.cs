b5fbe660590e620f23022d79263040bcf6b5ecf9
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-26 22:46:58 +0300

    Added user entity

---
+++ FoodApp/Constants/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Constants
+{
+    public enum UserRoles
+    {
+        CUSTOMER,
+        ADMIN
+    }
+}
---
+++ FoodApp/Models/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using FoodApp.Constants;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Surname { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+        [MaxLength(12)]
+        public string PhoneNumber { get; set; }
+        public UserRoles Role { get; set; }
+        //public IEnumerable<IBillingProfile> Billing { get; set; }
+        //public IEnumerable<Address> Addresses { get; set; }
+    }
+}
