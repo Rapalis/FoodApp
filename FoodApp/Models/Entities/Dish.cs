e3f233313715606e06fe77fcc4c2c6f077384446
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-26 23:00:54 +0300

    Added dish entity

---
+++ FoodApp/Models/Entities/Dish.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodApp.Models
+{
+    public class Dish
+    {
+        public long Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        [MaxLength(4000)]
+        public string Description { get; set; }
+        [Required]
+        public long ProviderId { get; set; }
+        public Provider Provider { get;set; }
+        public IEnumerable<Review> Reviews { get; set; } = new List<Review>();
+    }
+}
