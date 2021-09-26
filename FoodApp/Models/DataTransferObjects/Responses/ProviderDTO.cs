b198ba3188b014800fd55ac4a8d577c8d722b65f
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-26 23:01:22 +0300

    Updated provider data models

--- FoodApp/Models/DataTransferObjects/Responses/ProviderDTO.cs
+++ FoodApp/Models/DataTransferObjects/Responses/ProviderDTO.cs
@@ -2,7 +2,9 @@
 {
     public class ProviderDTO
     {
+        public string Email { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
         public string Address { get; set; }
     }
 }
--- FoodApp/Models/Entities/Provider.cs
+++ FoodApp/Models/Entities/Provider.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FoodApp.Models
 {
     public class Provider
     {
         public long Id { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Address { get; set; }
-        // ServedDiches
+        public IEnumerable<Dish> Servings { get; set; } = new List<Dish>();
     }
 }
