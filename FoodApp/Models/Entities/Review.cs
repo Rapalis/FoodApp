4c61becba54513d7496954101555ded1a9c49408
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-26 23:00:12 +0300

    Added review entity

---
+++ FoodApp/Models/Entities/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodApp.Models
+{
+    public class Review
+    {
+        public long Id { get; set; }
+        [Required]
+        public DateTimeOffset Date { get; set; }
+        [Required]
+        [MaxLength(4000)]
+        public string Message { get; set; }
+        [Required]
+        public short Score { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        [Required]
+        public long DishID { get; set; }
+        public Dish Dish { get; set; }
+    }
+}
