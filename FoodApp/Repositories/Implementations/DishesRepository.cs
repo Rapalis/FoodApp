3812e7a1f486aaf44340d285513476bfc93e00dc
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-29 22:52:12 +0300

    Added dishes repository

---
+++ FoodApp/Repositories/Implementations/DishesRepository.cs
@@ -0,0 +1,25 @@
+﻿using FoodApp.DataAccess;
+using FoodApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Repositories
+{
+    public class DishesRepository : BaseRepository<Dish>, IDishesRepository
+    {
+        private readonly DatabaseContext _dbCtx;
+
+        public DishesRepository(DatabaseContext dbCtx) : base(dbCtx)
+        {
+            _dbCtx = dbCtx;
+        }
+
+        public async Task<IEnumerable<Dish>> GetAllAsync(long providerId)
+        {
+             return await _dbCtx.Dish.Where(d => d.ProviderId == providerId).ToListAsync();
+        }
+    }
+}
---
+++ FoodApp/Repositories/Interfaces/IDishesRepository.cs
@@ -0,0 +1,13 @@
+﻿using FoodApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Repositories
+{
+    public interface IDishesRepository : IBaseRepository<Dish>
+    {
+        Task<IEnumerable<Dish>> GetAllAsync(long providerId); 
+    }
+}
