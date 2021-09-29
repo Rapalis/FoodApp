8fdbb0e54c4a8e085a1d3d822078f5a714bf6e08
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-29 22:52:44 +0300

    Added dishes mapping profile

---
+++ FoodApp/Profiles/DishProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FoodApp.Models;
+
+namespace FoodApp.Profiles
+{
+    public class DishProfile: Profile
+    {
+        public DishProfile()
+        {
+            CreateMap<CreateDishDTO, Dish>();
+            CreateMap<Dish, DishDTO>();
+        }
+    }
+}
