bfcd945ef24e41771e5590c837774afcebbc3cb6
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-30 23:53:14 +0300

    Added review DTOs

---
+++ FoodApp/Models/DataTransferObjects/Requests/CreateReviewDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Models.DataTransferObjects
+{
+    public class CreateReviewDTO
+    {
+        public DateTimeOffset Date { get; set; }
+        public string Message { get; set; }
+        public short Score { get; set; }
+    }
+}
---
+++ FoodApp/Models/DataTransferObjects/Responses/ReviewDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Models.DataTransferObjects
+{
+    public class ReviewDTO
+    {
+        public long Id { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string Message { get; set; }
+        public short Score { get; set; }
+    }
+}
