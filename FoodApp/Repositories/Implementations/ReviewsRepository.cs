8e610c4eebc0db480c69bcefa6c501661822503a
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-09-30 23:52:41 +0300

    Added reviews repository

---
+++ FoodApp/Repositories/Implementations/ReviewsRepository.cs
@@ -0,0 +1,25 @@
+﻿using FoodApp.DataAccess;
+using FoodApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Repositories.Implementations
+{
+    public class ReviewsRepository: BaseRepository<Review>, IReviewsRepository
+    {
+        private readonly DatabaseContext _dbCtx;
+
+        public ReviewsRepository(DatabaseContext dbCtx) : base(dbCtx)
+        {
+            _dbCtx = dbCtx;
+        }
+
+        public async Task<IEnumerable<Review>> GetAllAsync(long dishId)
+        {
+            return await _dbCtx.Review.Where(d => d.DishID == dishId).ToListAsync();
+        }
+    }
+}
---
+++ FoodApp/Repositories/Interfaces/IReviewsRepository.cs
@@ -0,0 +1,13 @@
+﻿using FoodApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Repositories
+{
+    public interface IReviewsRepository: IBaseRepository<Review>
+    {
+        Task<IEnumerable<Review>> GetAllAsync(long dishId);
+    }
+}
